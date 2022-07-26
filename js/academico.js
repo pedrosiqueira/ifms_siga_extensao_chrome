5f1559c50aa2e515d6a44a3f2a04b29b23ccfc01
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2022-07-26 17:54:47 -0400

    a função fetchHTML é comum tanto a academico quanto a suap, portanto a mudei para um script comum aos dois

--- js/academico.js
+++ js/academico.js
@@ -1,18 +1,5 @@
 let contador; // variável que conta quantas requisições ajax foram feitas
 
-async function fetchHTML(html) {
-    html = await fetch(chrome.runtime.getURL("html/" + html + ".html"));
-    html = await html.text();
-    document.body.insertAdjacentHTML('beforeend', html);
-}
-
-function htmlToElement(html) {
-    let template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    template.innerHTML = html;
-    return template.content.firstChild;
-}
-
 function sendMessage(funcao, dados, callback) {
     chrome.runtime.sendMessage({ funcao: funcao, dados: dados }, callback);
 }
