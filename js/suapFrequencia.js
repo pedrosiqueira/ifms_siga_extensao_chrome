96fb1d6cbbcee56e85d5515ee663b7dece437dd1
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2023-03-11 17:02:47 -0400

    imprime no terminal os horários de saída após as 22h

--- js/suapFrequencia.js
+++ js/suapFrequencia.js
@@ -1,3 +1,26 @@
+function diffTime(t1, t2) {
+    t1 = t1.split(":").map(str => parseInt(str))
+    t2 = t2.split(":").map(str => parseInt(str))
+    return (t2[0] - t1[0]).toString().padStart(2, "0") + ":" + (t2[1] - t1[1]).toString().padStart(2, "0") + ":" + (t2[2] - t1[2]).toString().padStart(2, "0")
+}
+
+function logSaidas() {
+    table = document.querySelector('#tabela-frequencias tbody')
+    saidas = ""
+    for (let i = 0; i < table.rows.length; i++) {
+        spans = table.rows[i].cells[2].querySelectorAll('span');
+        saida = spans[spans.length - 1].innerText
+        horas = parseInt(saida.substring(0, 2))
+        if (horas >= 0 && horas <= 1) saida = (horas + 24) + saida.substring(2) // adicional noturno é para horário de 22:00:00 pra cima
+        if (horas < 22 || isNaN(horas)) continue
+        data = table.rows[i].cells[1].querySelector('strong').innerText
+        dia = table.rows[i].cells[1].querySelectorAll('p')[1].innerText
+        tempo = diffTime("22:00:00", saida)
+        saidas += data + "\t" + dia + "\t" + saida + "\t" + tempo + "\n"
+    }
+    console.log(saidas)
+}
+
 function changeDisplay(display) {
     if (document.getElementById("breadcrumbs")) document.getElementById("breadcrumbs").style.display = display;
     document.querySelectorAll("h3").forEach(el => { if (el.textContent.includes('Filtros')) { el.parentElement.style.display = display; } });
@@ -21,6 +44,10 @@ function imprimido() {
 async function init() {
     window.onbeforeprint = imprimir;
     window.onafterprint = imprimido;
+
+    let btnLogSaidas = htmlToElement('<button class="btn" style="margin-left: 10px" ><span class="fas fa-terminal" aria-hidden="true"></span> Log saídas noturnas</button>');
+    document.querySelector(".mostrarTodasObservacoes").insertAdjacentElement("afterend", btnLogSaidas);
+    btnLogSaidas.onclick = logSaidas;
 }
 
 window.addEventListener('load', (event) => { init(); }); // só executa o script depois q a página terminou de ser carregada
